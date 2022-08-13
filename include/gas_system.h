6f04f0ae5b4d4fa25ac9a6e37d383aab6b4f2343
bobsayshilol <bobsayshilol@live.co.uk>
2022-08-13 02:09:44 +0100

    Add a user defined constructor for Mix
    I'm not 100% sure why GCC wants this but it has something to do with
    aggregate initialisation (= {}) and default values for members.

--- include/gas_system.h
+++ include/gas_system.h
@@ -10,6 +10,7 @@
 class GasSystem {
     public:
         struct Mix {
+            Mix() {} // Ctor must exist and can't be =default if used as an aggregate (= {})
             double p_fuel = 0.0;
             double p_inert = 1.0;
             double p_o2 = 0.0;
