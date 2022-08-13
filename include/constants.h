1e0a2e0afcd7f51c2447956d772b1d6a5d0f1dad
bobsayshilol <bobsayshilol@live.co.uk>
2022-08-13 02:04:27 +0100

    Replace extern with static
    constexpr is for values known at compile time so there's no reason for
    these to be extern since they can't be defined externally.

--- include/constants.h
+++ include/constants.h
@@ -3,10 +3,10 @@
 
 namespace constants {
 
-    extern constexpr double pi = 3.14159265359;
-    extern constexpr double R = 8.31446261815324;
-    extern constexpr double root_2 = 1.41421356237309504880168872420969807856967187537694807317667973799;
-    extern constexpr double e = 2.71828182845904523536028747135266249775724709369995;
+    static constexpr double pi = 3.14159265359;
+    static constexpr double R = 8.31446261815324;
+    static constexpr double root_2 = 1.41421356237309504880168872420969807856967187537694807317667973799;
+    static constexpr double e = 2.71828182845904523536028747135266249775724709369995;
 
 } /* namespace Constants */
 
