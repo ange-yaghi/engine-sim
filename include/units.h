210b4f5988b75c23a911a05745a0c9fb469173d0
bobsayshilol <bobsayshilol@live.co.uk>
2022-08-13 02:04:27 +0100

    Replace extern with static
    constexpr is for values known at compile time so there's no reason for
    these to be extern since they can't be defined externally.

--- include/units.h
+++ include/units.h
@@ -5,94 +5,94 @@
 
 namespace units {
     // Force
-    extern constexpr double N = 1.0;
+    static constexpr double N = 1.0;
 
-    extern constexpr double lbf = N * 4.44822;
+    static constexpr double lbf = N * 4.44822;
 
     // Mass
-    extern constexpr double kg = 1.0;
-    extern constexpr double g = kg / 1000.0;
+    static constexpr double kg = 1.0;
+    static constexpr double g = kg / 1000.0;
 
-    extern constexpr double lb = 0.45359237 * kg;
+    static constexpr double lb = 0.45359237 * kg;
 
     // Distance
-    extern constexpr double m = 1.0;
-    extern constexpr double cm = m / 100.0;
-    extern constexpr double mm = m / 1000.0;
-    extern constexpr double km = m * 1000.0;
+    static constexpr double m = 1.0;
+    static constexpr double cm = m / 100.0;
+    static constexpr double mm = m / 1000.0;
+    static constexpr double km = m * 1000.0;
 
-    extern constexpr double inch = cm * 2.54;
-    extern constexpr double foot = inch * 12.0;
-    extern constexpr double thou = inch / 1000.0;
+    static constexpr double inch = cm * 2.54;
+    static constexpr double foot = inch * 12.0;
+    static constexpr double thou = inch / 1000.0;
 
-    extern constexpr double mile = m * 1609.344;
+    static constexpr double mile = m * 1609.344;
 
     // Time
-    extern constexpr double sec = 1.0;
-    extern constexpr double minute = 60 * sec;
-    extern constexpr double hour = 60 * minute;
+    static constexpr double sec = 1.0;
+    static constexpr double minute = 60 * sec;
+    static constexpr double hour = 60 * minute;
 
     // Torque
-    extern constexpr double Nm = N * m;
-    extern constexpr double ft_lb = foot * lbf;
+    static constexpr double Nm = N * m;
+    static constexpr double ft_lb = foot * lbf;
 
     // Power
-    extern constexpr double W = Nm / sec;
-    extern constexpr double kW = W * 1000.0;
-    extern constexpr double hp = 745.699872 * W;
+    static constexpr double W = Nm / sec;
+    static constexpr double kW = W * 1000.0;
+    static constexpr double hp = 745.699872 * W;
 
     // Volume
-    extern constexpr double m3 = 1.0;
-    extern constexpr double cc = cm * cm * cm;
-    extern constexpr double mL = cc;
-    extern constexpr double L = mL * 1000.0;
-    extern constexpr double cubic_feet = foot * foot * foot;
-    extern constexpr double cubic_inches = inch * inch * inch;
-    extern constexpr double gal = 3.785411784 * L;
+    static constexpr double m3 = 1.0;
+    static constexpr double cc = cm * cm * cm;
+    static constexpr double mL = cc;
+    static constexpr double L = mL * 1000.0;
+    static constexpr double cubic_feet = foot * foot * foot;
+    static constexpr double cubic_inches = inch * inch * inch;
+    static constexpr double gal = 3.785411784 * L;
 
     // Molecular
-    extern constexpr double mol = 1.0;
-    extern constexpr double kmol = mol / 1000.0;
-    extern constexpr double mmol = mol / 1000000.0;
-    extern constexpr double lbmol = mol * 453.59237;
+    static constexpr double mol = 1.0;
+    static constexpr double kmol = mol / 1000.0;
+    static constexpr double mmol = mol / 1000000.0;
+    static constexpr double lbmol = mol * 453.59237;
 
     // Flow-rate (moles)
-    extern constexpr double mol_per_sec = mol / sec;
-    extern constexpr double scfm = 0.002641 * lbmol / minute;
+    static constexpr double mol_per_sec = mol / sec;
+    static constexpr double scfm = 0.002641 * lbmol / minute;
 
     // Area
-    extern constexpr double m2 = 1.0;
-    extern constexpr double cm2 = cm * cm;
+    static constexpr double m2 = 1.0;
+    static constexpr double cm2 = cm * cm;
 
     // Pressure
-    extern constexpr double Pa = 1.0;
-    extern constexpr double kPa = Pa * 1000.0;
-    extern constexpr double MPa = Pa * 1000000.0;
-    extern constexpr double atm = 101.325 * kPa;
+    static constexpr double Pa = 1.0;
+    static constexpr double kPa = Pa * 1000.0;
+    static constexpr double MPa = Pa * 1000000.0;
+    static constexpr double atm = 101.325 * kPa;
 
-    extern constexpr double mbar = Pa * 100.0;
-    extern constexpr double bar = mbar * 1000.0;
+    static constexpr double mbar = Pa * 100.0;
+    static constexpr double bar = mbar * 1000.0;
 
-    extern constexpr double psi = lbf / (inch * inch);
-    extern constexpr double psig = psi;
-    extern constexpr double inHg = Pa * 3386.3886666666713;
-    extern constexpr double inH2O = inHg * 0.0734824;
+    static constexpr double psi = lbf / (inch * inch);
+    static constexpr double psig = psi;
+    static constexpr double inHg = Pa * 3386.3886666666713;
+    static constexpr double inH2O = inHg * 0.0734824;
 
     // Temperature
-    extern constexpr double K = 1.0;
-    extern constexpr double K0 = 273.15;
-    extern constexpr double C = K;
-    extern constexpr double F = (5.0 / 9.0) * K;
-    extern constexpr double F0 = -459.67;
+    static constexpr double K = 1.0;
+    static constexpr double K0 = 273.15;
+    static constexpr double C = K;
+    static constexpr double F = (5.0 / 9.0) * K;
+    static constexpr double F0 = -459.67;
 
     // Energy
-    extern constexpr double J = 1.0;
-    extern constexpr double kJ = J * 1000;
-    extern constexpr double MJ = J * 1000000;
+    static constexpr double J = 1.0;
+    static constexpr double kJ = J * 1000;
+    static constexpr double MJ = J * 1000000;
 
     // Angles
-    extern constexpr double rad = 1.0;
-    extern constexpr double deg = rad * (constants::pi / 180);
+    static constexpr double rad = 1.0;
+    static constexpr double deg = rad * (constants::pi / 180);
 
     // Conversions
     inline constexpr double distance(double v, double unit) {
