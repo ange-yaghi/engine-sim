9b610b87851c164a12a4b4fcbd4123b95e3b246b
AKCore <AKCore@users.noreply.github.com>
2022-08-15 00:32:15 -0700

    Add Discord Rich Presence. (#2)
    Displays Engine Name, Build Version, and Time since start.

---
+++ dependencies/discord/Discord.h
@@ -0,0 +1,82 @@
+﻿/*****************************************************************************
+*
+* File			: Discord.h
+* Author		: SanGawku
+* Copyright		: SanGawku
+* Date			: 7/29/2019
+* Abstract		: Discord
+*****************************************************************************
+*
+* Desc         : Discord rich presence integration
+*
+*****************************************************************************/
+#ifndef __DISCORD_H__
+#define __DISCORD_H__
+
+#include <string>
+#include "discord_rpc.h"
+#pragma comment(lib, "discord-rpc.lib")
+
+class CDiscord
+{
+
+public:
+
+	CDiscord();
+	virtual ~CDiscord();
+
+	static bool		CreateInstance();
+	static void		DestroyInstance();
+
+	static CDiscord* GetInstance() { return m_pInstance; }
+
+public:
+
+	static void handleDiscordReady(const DiscordUser* connectedUser);
+
+	static void handleDiscordDisconnected(int errcode, const char* message);
+
+	static void handleDiscordError(int errcode, const char* message);
+
+private:
+
+	void InitDiscord();
+
+	void UpdatePresence();
+
+	void ClearPresence();
+
+public:
+
+	void			UpdateDiscordConnection();
+
+	void			SetUseDiscord(bool bFlag); // if true, then we will use discord and connect to discord
+
+	void			SetConnected(bool bFlag); //set connected to discord or not
+
+	void			SetStatus(DiscordRichPresence presence, std::string engineName, std::string buildVersion);
+
+
+
+private:
+
+	std::string		m_state;
+	
+	std::string		m_engineName;
+
+	bool			m_bEnableDiscord;
+
+	bool			m_bConnected;
+
+protected:
+
+	static CDiscord* m_pInstance;
+};
+
+static CDiscord* GetDiscordManager()
+{
+	return CDiscord::GetInstance();
+};
+
+
+#endif
--- src/engine_sim_application.cpp
+++ src/engine_sim_application.cpp
@@ -20,7 +20,13 @@
 #include <stdlib.h>
 #include <sstream>
 
+
+#if ATG_ENGINE_DISCORD_ENABLED
+#include "../discord/Discord.h"
+#endif
+
 std::string EngineSimApplication::s_buildVersion = "0.1.6a";
+
 
 EngineSimApplication::EngineSimApplication() {
     m_assetPath = "";
@@ -261,6 +267,16 @@ void EngineSimApplication::initialize() {
     m_audioSource->SetMode(ysAudioSource::Mode::Loop);
     m_audioSource->SetPan(0.0f);
     m_audioSource->SetVolume(1.0f);
+
+#ifdef ATG_ENGINE_DISCORD_ENABLED
+    //Create a global instance of discord-rpc
+    CDiscord::CreateInstance();
+    //Enable it, this needs to be set via a config file of some sort. 
+    GetDiscordManager()->SetUseDiscord(true);
+    DiscordRichPresence passMe = { 0 };
+    GetDiscordManager()->SetStatus(passMe, m_iceEngine->getName(), s_buildVersion);
+#endif
+
 }
 
 void EngineSimApplication::process(float frame_dt) {
