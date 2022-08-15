a3cd6d03a3254c9094e1deb50b159ef7fe574a03
bobsayshilol <bobsayshilol@live.co.uk>
2022-08-16 00:06:06 +0100

    Add a main() and common app running code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "../include/engine_sim_application.h"
 
-#include <iostream>
+static void runApp(void *handle) {
+    EngineSimApplication application;
+    application.initialize(handle, ysContextObject::DeviceAPI::OpenGL4_0);
+    application.run();
+    application.destroy();
+}
 
+#if _WIN32
 int WINAPI WinMain(
     _In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
@@ -12,10 +18,15 @@ int WINAPI WinMain(
     (void)lpCmdLine;
     (void)hPrevInstance;
 
-    EngineSimApplication application;
-    application.initialize((void *)&hInstance, ysContextObject::DeviceAPI::DirectX11);
-    application.run();
-    application.destroy();
+    runApp(static_cast<void*>(&hInstance));
 
     return 0;
 }
+
+#else
+int main() {
+    runApp(nullptr);
+    return 0;
+}
+
+#endif
