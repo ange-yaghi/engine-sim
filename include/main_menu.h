009b187185f72a26025564df9f79bb0067d15731
Vrobocio <87225187+DDev247@users.noreply.github.com>
2022-10-23 12:11:06 +0200

    Support for older engines
    Added support for engines without `main()` and cleaned up the code a little

--- include/main_menu.h
+++ include/main_menu.h
@@ -7,49 +7,46 @@
 
 class EngineSimApplication;
 enum class MainMenuSceneType {
-	MainMenu,
-	Change,
-	Set
+    MainMenu,
+    Change,
+    Set
 };
 
 class MainMenu {
 public:
-	MainMenu();
-	~MainMenu();
+    MainMenu();
+    ~MainMenu();
 
-	static MainMenu Create(EngineSimApplication *app);
-
-public:
-	void SetApp(EngineSimApplication* app);
+    static MainMenu Create(EngineSimApplication *app);
 
 public:
-	void render();
-	void process();
+    void SetApp(EngineSimApplication* app);
 
-	void setTransform(float scale, float lx, float ly, float angle);
-	void resetShaders();
+public:
+    void render();
+    void process();
 
-	void getEngines();
-	bool checkExists(int index);
-	void setEngine(int index);
+    void setTransform(float scale, float lx, float ly, float angle);
+    void resetShaders();
+
+    void getEngines();
+    bool checkExists(int index);
+    void setEngine(int index);
 
 protected:
-	EngineSimApplication *m_app;
-	dbasic::DeltaEngine *m_engine;
-	MainMenuSceneType m_sceneType = MainMenuSceneType::MainMenu;
-	
-	int m_mouseX = 0;
-	int m_mouseY = 0;
+    EngineSimApplication *m_app;
+    dbasic::DeltaEngine *m_engine;
+    MainMenuSceneType m_sceneType = MainMenuSceneType::MainMenu;
 
-	std::map<std::string, std::pair<std::string, std::filesystem::path>> m_engines;
-	std::vector<std::string> m_man;
-	int m_selectedMan = 0;
-	int m_manCount = 0;
+    std::map<std::pair<std::string, bool>, std::pair<std::string, std::filesystem::path>> m_engines;
+    std::vector<std::string> m_man;
+    int m_selectedMan = 0;
+    int m_manCount = 0;
 
-	int m_changePage = 0;
-	int m_setPage = 0;
+    int m_changePage = 0;
+    int m_setPage = 0;
 
-	int m_selectedEngine = 0;
+    int m_selectedEngine = 0;
 };
 
 #endif /* ATG_ENGINE_SIM_MAIN_MENU_H */
